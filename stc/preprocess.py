3d14f11924edaea8754b1bf87e4d03116e00acfc
Omar <69389901+0x1331@users.noreply.github.com>
2025-03-06 00:04:54 +0400

    Update preprocess.py

--- stc/preprocess.py
+++ stc/preprocess.py
@@ -1,7 +1,6 @@
 from transformers import GPT2Tokenizer
 from datasets import load_dataset
 
-# Загрузка токенизатора GPT-2
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 # Чтение датасета
@@ -12,7 +11,6 @@ def preprocess_data(input_file, output_file):
     # Токенизация данных
     encodings = tokenizer("\n".join(texts), truncation=True, padding=True, max_length=512)
 
-    # Сохранение обработанных данных
     with open(output_file, 'w', encoding='utf-8') as f:
         for encoding in encodings['input_ids']:
             f.write(f"{encoding}\n")
